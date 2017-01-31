ed7385730269c82538cb0b2242c4f7d3b9d9fb00
Shohei Nakayama <nakker1218@gmail.com>
2017-01-31 12:11:34 +0900

    ライブラリを削除

--- app/src/main/java/com/shohei/rotateprogresslibraryapp/MainActivity.java
+++ app/src/main/java/com/shohei/rotateprogresslibraryapp/MainActivity.java
@@ -5,22 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.shohei.rotateprogress.RotateProgressBar;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-
 public class MainActivity extends AppCompatActivity {
-    @BindView(R.id.rotate_progress_bar)
-    RotateProgressBar rotateProgressBuilder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
+        RotateProgressBar rotateProgressBar = (RotateProgressBar) findViewById(R.id.rotate_progress_bar);
 
 //        rotateProgressBuilder.setColors(Color.RED, Color.BLUE, Color.GREEN, Color.CYAN);
 //        rotateProgressBuilder.setColors("#F44336", "#E91E63", "#9C27B0", "#673AB7", "#3F51B5", "#2196F3", "#03A9F4", "#00BCD4", "#009688", "#4CAF50", "#8BC34A", "#CDDC39", "#FFC107", "#FF9800", "#FF5722", "#795548", "#9E9E9E", "#607D8B");
-        rotateProgressBuilder.setColorsResource(R.color.colorPrimary, R.color.colorAccent, R.color.colorPrimaryDark);
-        rotateProgressBuilder.start();
+        rotateProgressBar.setColorsResource(R.color.colorPrimary, R.color.colorAccent, R.color.colorPrimaryDark);
+        rotateProgressBar.start();
     }
 }
